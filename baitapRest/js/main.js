3a77e30ac9b1e8b5575da404da427709a427ea87
Minh Tâm <minhtam130501@gmail.com>
2022-09-03 22:34:18 +0700

    up

--- baitapRest/js/main.js
+++ baitapRest/js/main.js
@@ -2,16 +2,7 @@ function getEle(id) {
     return document.getElementById(id);
 };
 
-const avg = (...numbers) => {
-    let sum = 0;
-    let diemTB = 0;
-    for (let index = 0; index < numbers.length; index++) {
-        sum += numbers[index];
-        diemTB = avg / numbers.length;
-    }
-};
-
-let toan = getEle("inpToan").value * 1;
+let toan = getEle('inpToan').value * 1;
 let ly = getEle('inpLy').value * 1;
 let hoa = getEle('inpHoa').value * 1;
 let van = getEle('inpVan').value * 1;
@@ -19,9 +10,18 @@ let su = getEle('inpSu').value * 1;
 let dia = getEle('inpDia').value * 1;
 let english = getEle('inpEnglish').value * 1;
 
+const avg = (...numbers) => {
+    let sum = 0;
+    let diemTB = 0;
+    for (let index = 0; index < numbers.length; index++) {
+        sum += numbers[index];
+        diemTB = sum / numbers.length;
+    }
+    console.log(diemTB);
+};
 
 document.getElementById('btnKhoi1').onclick = () => {
-    document.getElementById('tbKhoi1').innerHTML = avg(toan, ly,hoa);
+    document.getElementById('tbKhoi1').innerHTML = avg(toan,ly,hoa);
 };
 document.getElementById('btnKhoi2').onclick = () => {
     document.getElementById('tbKhoi2').innerHTML = avg(van, su, dia, english);
